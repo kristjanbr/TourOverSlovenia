29898204d1c4528dd4aa51a92c7189397179787c
kristjTSC <kristjanbratasevec1@gmail.com>
2021-10-21 20:46:39 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TourOverSlovenia
+
+The website is avaible at https://kristjTSC.github.io/TourOverSlovenia
---
+++ js/main.js
@@ -0,0 +1,33 @@
+var timezone = (new Date()).getTimezoneOffset() * 60000; //offset timezona v milisek, če tega ni, jemlje UK čas
+var today = new Date((Date.now() - timezone)).toISOString().slice(0, 10);
+document.getElementById("end").min = today;
+document.getElementById("start").min = today;
+
+document.getElementById("start").addEventListener("change", function () {
+    var input = this.value;
+    document.getElementById("end").min = input;
+    if (document.getElementById("end").value < document.getElementById("start").value) {
+        document.getElementById("end").value = document.getElementById("start").value;
+    }
+});
+
+fullForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Swal.fire({
+        icon: 'success',
+        title: 'Form submitted!',
+        text: 'Thanks, '+document.getElementById("fName").value+'! We will contact you within 24 hours!',
+        footer: '<a href="https://www.slovenia.info/en" style="color: #575fcf;">In the meantime you can visit the official travel guide to Slovenia</a>',
+    }).then((result) => {
+            document.getElementById('fullForm').submit();
+    });
+});
+function about(){
+    Swal.fire({
+        position: 'top',
+        title: 'Credits',
+        text: 'Made by Kristjan Brataševec, 4.RA',
+        icon: 'info',
+        showConfirmButton: false,
+    })
+}
